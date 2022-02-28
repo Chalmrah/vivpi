1f6510822015649c9f5b9c7118b728cdc4c4387d
Chalmrah <Francis@lark.me.uk>
2022-02-28 00:55:36 +0000

    removing ansible

---
+++ install.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+#
+# install.sh 
+#
+# Insall script for downloading and installing the vivpi system and associated services
+#
+
+# Variables
+installLocation="/etc/vivpi"
+
+# Initialisation
+echo -e ""
+echo -e "Vivpi Installation"
+echo -e ""
+
+if [ "$EUID" -ne 0 ]
+  then echo "! Please run as root to ensure install completes correctly!"
+  exit
+fi
+
+# Download git to temp location
+tempDir=$(mktemp -d)
+git clone --depth 1 https://github.com/Chalmrah/vivpi.git $tempDir
+
+origDir=$(pwd)
+cd "$tempDir"
+
+if [ readlink -e $installLocation ]
+then
+  echo -e "> First time install"
+  echo -e ">  Prerequisites"
+  apt install python3 python3-pip python-dev -y
+  pip3 install -y -r $tempDir/requirements.txt
+  mkdir $installLocation
+else
+  echo -e "> Upgrade mode"
+fi
+
+echo -e ">  Copying files"
+cp -ur vivpi/ $installLocation
+
+echo -e ">  Installing services"
+for service in `ls -d $tempDir/services/*`; do
+  sed -i "s#install_Location#${installLocation}#g" $service
+  cp -u $service /etc/systemd/system/
+  systemctl enable /etc/systemd/system/$service
+done
+
+echo -e "Reloading systemctl"4
+systemctl daemon-reload
+
+cd "$origDir"
---
+++ vivpi/src/config.py
@@ -0,0 +1,86 @@
+import json
+import os
+from jsonschema import validate, ValidationError, SchemaError
+import src.logging as log
+
+
+def validateConfig(settings):
+    schema = {
+        "type" : "object",
+        "data": {
+            "type": "object",
+            "properties" : {
+                "sensorIpAddress" : {"type" : "string"},
+                "thingspeakKey" : {"type" : "string",},
+                "thingspeakEnabled" : {"type" : "boolean"}
+            },
+            "required": ["sensorIpAddress","thingspeakEnabled"]
+        },
+        "temperatures": {
+            "type": "object",
+            "properties" : {
+                "maxDayTemp" : {"type" : "number",},
+                "maxNightTemp" : {"type" : "number",},
+                "minDayTemp" : {"type" : "number",},
+                "minNightTemp" : {"type" : "number",}
+            },
+            "required": ["dayTemp","nightTemp"]
+        },
+        "humidity": {
+            "type": "object",
+            "properties" : {
+                "maxHumidity" : {"type" : "number",},
+                "minHumidity" : {"type" : "number",},
+                "useColdHumidity" : {"type" : "boolean"},
+                "useTimedMister" : {"type" : "boolean"},
+                "timedMisterSeconds" : {"type" : "number"},
+                "ignoreMister" : {"type" : "boolean"}
+            },
+            "required": ["humidityMax","humidityMin"]
+        },
+        "time": {
+            "type": "object",
+            "properties" : {
+                "dayStart" : {"type" : "number",},
+                "nightStart" : {"type" : "number",}
+            },
+            "required": ["dayStart","nightStart"]
+        },
+        "switches": {
+            "type": "object",
+            "properties" : {
+                "tempSwitch" : {"type" : "number",},
+                "humSwitch" : {"type" : "number",},
+                "lightSwitch" : {"type" : "number",}
+            },
+            "required": ["tempSwitch","humSwitch"]
+        },
+        "logging": {
+            "type": "object",
+            "properties" : {}
+        },
+        "required": ["data","temperatures","humidity","time","switches"]
+    }
+    try:
+        validate(settings, schema)
+    except SchemaError as e:
+        log.logInfo("There is an error with the schema")
+        return False
+    except ValidationError as e:
+        log.logEmerg(e)
+        log.logEmerg(e.absolute_path)
+        return False
+    return True
+
+def load():
+    dir = os.path.dirname(__file__)
+
+    with open(os.path.join(dir, '..', 'config.json'), mode='r', encoding='utf-8') as f:
+        return json.load(f)
+
+def write(settings):
+    dir = os.path.dirname(__file__)
+
+    with open(os.path.join(dir, '..', 'config.json'),'w') as f:
+        json.dump(settings, f, indent=4)
+    return "Settings updated"
