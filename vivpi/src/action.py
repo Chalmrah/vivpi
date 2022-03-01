0f5dfe7498aaf24753f61128bb2eb627df66c208
Chalmrah <Francis@lark.me.uk>
2022-03-01 21:28:23 +0000

    added alerting to humidity and temperature

--- vivpi/src/action.py
+++ vivpi/src/action.py
@@ -17,8 +17,6 @@ def isDuringDay(dayStart, nightStart, currentTime):
 def verifyTemperature(data):
     temp = float(data['warmTemperature'])
     isDay = isDuringDay(settings['time']['dayStart'],settings['time']['nightStart'],currentHour)
-    # Default to nothing 
-    action = None
     if isDay:
         maxTemp = float(settings['temperatures']['maxDayTemp'])
         minTemp = float(settings['temperatures']['minDayTemp'])
@@ -26,9 +24,11 @@ def verifyTemperature(data):
         maxTemp = float(settings['temperatures']['maxNightTemp'])
         minTemp = float(settings['temperatures']['minNightTemp'])
     
-    alertTemp = minTemp - float(settings['temperatures']['alertThresholdTemp'])
-    log.logDebug("Using daytime thresholds of maxTemp: {}, minTemp: {}, and alertTemp: {}".format(maxTemp, minTemp, alertTemp))
-        
+    # Validating alerts
+    validateTemperatureAlerts(temp,minTemp,maxTemp)
+
+    # Default to nothing 
+    action = None
     if temp > maxTemp:
         log.logDebug("Temperature too high, turn off")
         action = False
@@ -36,9 +36,6 @@ def verifyTemperature(data):
     if temp < minTemp:
         log.logDebug("Temperature too low, turn on")
         action = True
-    
-    if temp < alertTemp:
-        telegram.sendAlert("Temperature Alert!\nCurrent temp: {}C\nAlert threshold: {}C\nCheck vivarium and heater are working!".format(temp, alertTemp))
 
     return action
 
@@ -49,6 +46,9 @@ def verifyHumidity(data):
         hum = float(data['warmHumidity'])
     minHum = float(settings['humidity']['minHumidity'])
     maxHum = float(settings['humidity']['maxHumidity'])
+
+    # Validating alerts
+    validateHumidityAlerts(hum,minHum,maxHum)
     # Default to do nothing
     action = None
     if hum > maxHum:
@@ -68,3 +68,26 @@ def verifyMisterOrFogger():
     else:
         switchNumber = settings['switches']['foggerSwitch']
     return switchNumber
+
+def validateTemperatureAlerts(temp,minTemp,maxTemp):
+    alertTemp = minTemp - float(settings['temperatures']['alertThresholdTemp'])
+    log.logDebug("Using thresholds of maxTemp: {}, minTemp: {}, and alertTemp: {}".format(maxTemp, minTemp, alertTemp))
+    # Giving a 10 minute breather after the day switch before alerting for temperature
+    breathingTime = 10
+    currentMinute = datetime.now().minute
+    if settings['time']['dayStart'] == currentHour and currentMinute < breathingTime:
+        pass
+    else:
+        if temp < alertTemp:
+            telegram.sendAlert("Temperature Alert!\nCurrent temp: {}C\nAlert threshold: {}C\nCheck vivarium and heater are working!".format(temp, alertTemp))
+
+def validateHumidityAlerts(hum,minHum,maxHum):
+    if maxHum+15 > 100:
+        maxAlertHum = 97
+    else:
+        maxAlertHum = maxHum+15
+    minAlertHum = minHum-20
+    if hum > maxAlertHum:
+        telegram.sendAlert("Humidity Alert!\nCurrent level: {}%\nAlert threshold: Above {}%\nCheck vivarium and mister are working!".format(hum, maxAlertHum))
+    if hum < minAlertHum:
+        telegram.sendAlert("Humidity Alert!\nCurrent level: {}%\nAlert threshold: Below {}%\nCheck vivarium and mister are working!".format(hum, minAlertHum))
