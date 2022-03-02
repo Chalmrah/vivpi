bc4e95ef6799cbe09e37c4f7f358f457b1428629
Chalmrah <Francis@lark.me.uk>
2022-03-02 00:33:05 +0000

    spacing

--- install.sh
+++ install.sh
@@ -10,9 +10,11 @@
 installLocation="/etc/vivpi"
 
 # Initialisation
+echo ""
 echo " =================="
 echo " Vivpi Installation"
 echo " =================="
+echo ""
 
 if [ $(id -u) -ne 0 ]
   then echo "! Please run as root to ensure install completes correctly!"
@@ -35,7 +37,7 @@ cd "$tempDir"
 if [ readlink -e $installLocation ] && [ systemctl is-active --quiet vivpi.timer ]
 then
   echo "> First time install"
-  echo ">  Prerequisites"
+  echo "> Prerequisites"
   apt install python3 python3-pip python-dev -y
   pip3 install -y -r $tempDir/requirements.txt
   mkdir $installLocation
@@ -43,10 +45,10 @@ else
   echo "> Upgrade mode"
 fi
 
-echo ">  Copying files"
+echo "> Copying files"
 cp -ur vivpi/ $installLocation
 
-echo ">  Installing services"
+echo "> Installing services"
 for service in `ls -d $tempDir/services/*`; do
   sed -i "s#install_Location#${installLocation}#g" $service
   cp -u $service /etc/systemd/system/
@@ -62,6 +64,7 @@ cd "$origDir"
 echo "> Cleaning up temp directory"
 rm -r $tempDir
 
+echo ""
 echo " ======================"
 echo " Installation Complete!"
 echo " ======================"
