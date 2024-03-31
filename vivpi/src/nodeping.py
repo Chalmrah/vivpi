178be91f88d33f5989a0156daddd2645060b113d
Chalmrah <Francis@lark.me.uk>
2024-03-31 16:34:54 +0100

    Changed check to just be heartbeat not data

--- vivpi/src/nodeping.py
+++ vivpi/src/nodeping.py
@@ -1,14 +1,15 @@
 import src.config as config
 import requests
+import json
 #import energenie
 
 settings = config.load()
 
-checkId = settings["data"]["nodepingCheckId"]
-checkToken = settings["data"]["nodepingCheckToken"]
-
 def postHeartbeat():
-  data = {'status': 200}
-  url = "https://push.nodeping.com/v1?id=%s&checktoken=%s" % checkId,checkToken
-  requests.post(url,data)
+  params = {
+    'id' : settings["data"]["nodepingCheckId"],
+    'checktoken' : settings["data"]["nodepingCheckToken"]
+    }
+  url = "https://push.nodeping.com/v1"
+  requests.post(url,params=params)
   
